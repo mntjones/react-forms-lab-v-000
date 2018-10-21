7918c586d99106b9477ed96a8f5164f87a537ea2
Monica Tamaru Jones <mntamaru@gmail.com>
2018-10-21 07:58:34 +0000

    automatically backed up by learn

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -30,7 +30,7 @@ class LoginForm extends React.Component {
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" onChange={this.handleChange}/>
           </label>
         </div>
         <div>
